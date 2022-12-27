b0ad56b4a4663db2af53b64d8ebfdb3641f9d982
Behdad BahramAbadian <57171421+lllBehdadlll@users.noreply.github.com>
2022-12-27 18:14:56 +0330

    Product category

--- OnlineShop/ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
+++ OnlineShop/ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -1,9 +1,18 @@
-﻿namespace ShopManagement.Domain.ProductCategoryAgg
+﻿using ShopManagement.Application.Contracts.ProductCategory;
+using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+
+namespace ShopManagement.Domain.ProductCategoryAgg
 {
     public interface IProductCategoryRepository
     {
         void Create(ProductCategory entity);
-        ProductCategory Get(int id);
+        ProductCategory Get(long id);
         List<ProductCategory> GetAll();
+        bool Exists(Expression<Func<ProductCategory, bool>> expression);
+        void SaveChanges();
+        EditProductCategory GetDtails(long id);
+        List<ProductCategoryViewModel> Search(ProductCategorySearchmodel searchmodel);
+        
     }
 }
