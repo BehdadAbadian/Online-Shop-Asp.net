899eec13ce4b26989fea0e764518b1491fd2c7e0
Behdad BahramAbadian <57171421+lllBehdadlll@users.noreply.github.com>
2022-12-29 20:53:11 +0330

    ProductCategory07-RepositoryBase,Configure

---
+++ OnlineShop/0-Framework/Infrastructure/RepositoryBase.cs
@@ -0,0 +1,42 @@
+﻿using _0_Framework.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using System.Linq.Expressions;
+
+namespace _0_Framework.Infrastructure
+{
+    public class RepositoryBase<TKey, T> : IRepository<TKey, T> where T : class
+    {
+        private readonly DbContext _context;
+
+        public RepositoryBase(DbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(T entity)
+        {
+            _context.Add(entity);
+        }
+
+        public bool Exists(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Any(expression);
+        }
+
+        public List<T> Get()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetT(TKey id)
+        {
+            return _context.Find<T>(id);
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
