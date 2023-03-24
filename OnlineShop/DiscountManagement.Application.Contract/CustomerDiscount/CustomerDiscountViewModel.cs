4278ed92ead4da1f970d3b4f8f7c830aca687adf
Behdad BahramAbadian <Behdad.abadian@gmail.com>
2023-03-24 09:27:26 +0330

    Discount Management 2
    DM.Domain
    DM.Application.Contract

---
+++ OnlineShop/DiscountManagement.Application.Contract/CustomerDiscount/CustomerDiscountViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DiscountManagement.Application.Contract.CustomerDiscount
+{
+    public class CustomerDiscountViewModel
+    {
+        public long ProductId { get; private set; }
+        public string Product { get; set; }
+        public int DiscountRate { get; private set; }
+        public string StartDate { get; private set; }
+        public string EndDate { get; private set; }
+        public string Reason { get; private set; }
+    }
+}
