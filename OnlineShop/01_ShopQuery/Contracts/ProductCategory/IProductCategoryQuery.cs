6ca7aaf6119c09dfe477db3a9174e11bbeb71f0b
Behdad BahramAbadian <Behdad.abadian@gmail.com>
2023-12-19 02:50:07 +0330

    UI category page
    V5

--- OnlineShop/01_ShopQuery/Contracts/ProductCategory/IProductCategoryQuery.cs
+++ OnlineShop/01_ShopQuery/Contracts/ProductCategory/IProductCategoryQuery.cs
@@ -4,6 +4,8 @@ namespace _01_ShopQuery.Contracts.ProductCategory
 {
     public interface IProductCategoryQuery
     {
+        ProductCategoryQueryModel GetProductCategoryWithProductsBy(string slug);
+
         List<ProductCategoryQueryModel> GetProductCategories();
         List<ProductCategoryQueryModel> GetProductCategoriesWithProduts();
     }
--- OnlineShop/01_ShopQuery/Query/ProductCategoryQuery.cs
+++ OnlineShop/01_ShopQuery/Query/ProductCategoryQuery.cs
@@ -115,5 +115,73 @@ namespace _01_ShopQuery.Query
 
             
         }
+
+        public ProductCategoryQueryModel GetProductCategoryWithProductsBy(string slug)
+        {
+            var inventory = _inventoryContext.Inventory.Select(x => new
+            {
+                x.ProductId,
+                x.UnitPrice,
+
+            }).ToList();
+
+            var discounts = _discountContext.CustomerDiscounts
+                .Where(x => x.StartDate < DateTime.Now && x.EndDate > DateTime.Now)
+                .Select(x => new { x.ProductId, x.DiscountRate, x.EndDate }).ToList();
+
+            var category = _context.ProductCategories
+                .Include(x => x.Products)
+                .ThenInclude(x => x.Category)
+                .Select(x => new ProductCategoryQueryModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description,
+                    MetaDescription = x.MetaDescription,
+                    Keywords = x.Keywords,
+                    Slug = x.Slug,
+                    Products = MapProducts(x.Products)
+                }).FirstOrDefault(x => x.Slug == slug);
+
+          
+                foreach (var product in category.Products)
+                {
+                    var productInventory = inventory.FirstOrDefault(x =>
+                    x.ProductId == product.Id);
+
+
+                    if (productInventory != null)
+                    {
+                        var price = productInventory.UnitPrice;
+
+                        product.Price = price.ToMoney();
+
+
+
+                        var discount = discounts.FirstOrDefault(x =>
+                         x.ProductId == product.Id);
+
+
+                        if (discount != null)
+                        {
+                            var discountRate = discount.DiscountRate;
+
+                            product.DiscountRate = discountRate;
+                            product.DiscountExpireDate = discount.EndDate.ToDiscountFormat();
+                            product.HasDiscount = discountRate > 0;
+                            var discountAmount = Math.Round((price * discountRate) / 100);
+
+                            product.PriceWithDiscount = (price - discountAmount).ToMoney();
+                        }
+                    }
+
+
+                }
+            
+
+
+
+            return category;
+        }
     }
 }
