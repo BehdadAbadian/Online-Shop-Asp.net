ac143f8143eb35918cf87afaa993be574bd4192d
Behdad BahramAbadian <Behdad.abadian@gmail.com>
2023-12-21 01:32:08 +0330

    UI Menu and Footer
    V6

---
+++ OnlineShop/ServiceHost/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace ServiceHost.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
